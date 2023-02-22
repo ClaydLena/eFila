e6dae7a5c3dd2fc718b1d51c4630b201ebecd6b3
ClaydLena <claydlena7@gmail.com>
2023-02-22 19:04:26 +0200

    updating styles at app

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import HoverRating from '../components/rating/Rating'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HoverRating/>
       </header>
     </div>
   );
