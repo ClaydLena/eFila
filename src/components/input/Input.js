31c36d1cf076283d8391852912b1e86431283d39
ClaydLena <claydlena7@gmail.com>
2023-02-22 13:46:35 +0200

    input component created

---
+++ src/components/input/Input.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import { makeStyles } from '@material-ui/core/styles';
+
+const getStyles = makeStyles(() => ({
+    input: {
+        marginBottom: '1%',
+        height: '70%',
+        width: '100%',
+    }
+}));
+
+function Input({ type, placeholder, onChange, label, value, min, max }) {
+    const classes = getStyles();
+
+    return (
+        <OutlinedInput
+            autoFocus={true}
+            className={classes.input}
+            type={type}
+            name={label}
+            value={value}
+            placeholder={placeholder}
+            onChange={onChange}
+            size='small'
+            min={min}
+            max={max}
+        />
+    )
+}
+
+export default Input
