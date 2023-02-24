7baf6bc85966812541b1f189318de805005532fb
ClaydLena <claydlena7@gmail.com>
2023-02-24 20:09:01 +0200

    layout component updated

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import { Queue } from "@material-ui/icons";
 
 const getStyles = makeStyles(() => ({
   layout: {
     width:'35%',
     height:'95vh',
+    color:'#333333 !important'
   },
   children:{
-    margin:'10vh 0 30vh 0'
+    margin:'10vh 0 25vh 0'
+  },
+  text:{
+    color:'#37C386',
+    fontWeight:'600px'
   }
 }));
 
@@ -18,11 +24,11 @@ function Layout({ children }) {
     return (
         <React.Fragment>
             <div className={classes.layout}>
-                <h2>Banco 123</h2>
+                <h2 className={classes.text}>Banco 123</h2>
                 <div className={classes.children}>
                     {children}
                 </div>
-                <p>
+                <p className={classes.text}>
                     Termos e condições aplicáveis - 2023 
                 </p>
             </div>
