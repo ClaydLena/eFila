f34c293ffdbdc3c19506dda2c358b7b0c2813725
ClaydLena <claydlena@gmail.com>
2023-07-04 20:24:11 +0200

    API service created

---
+++ src/api/apiService.js
@@ -0,0 +1,43 @@
+const API_URL = "http://localhost:8000"
+
+async function sendRequest(url, options) {
+    const response = await fetch(url, options)
+    return await response.json();
+}
+
+function createResquestOptions(method, body) {
+    return {
+        method,
+        headers: {
+            'Content-Type': 'applications/json',
+            'Access-Control-Allow-Origin':'*'
+        },
+        body: JSON.stringify(body)
+    };
+}
+
+async function fetchData(endpoit) {
+    return await sendRequest(`${API_URL}/${endpoit}`)
+}
+
+async function postData(endpoit, data) {
+    const options = createResquestOptions('POST', data)
+    return await sendRequest(`${API_URL}/${endpoit}`, options);
+}
+
+async function updateData(endpoit,id, data) {
+    const options = createResquestOptions('PUT', data)
+    return await sendRequest(`${API_URL}/${endpoit}/${id}`, options);
+}
+
+async function deleteData(endpoit,id) {
+    const options = createResquestOptions('DELETE')
+    return await sendRequest(`${API_URL}/${endpoit}/${id}`, options);
+}
+
+export default {
+    postData,
+    updateData,
+    fetchData,
+    deleteData,
+};
